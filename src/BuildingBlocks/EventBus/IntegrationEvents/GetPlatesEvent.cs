45093f5989f565edb14bafd36bb62db40da977d4
LoftyTowers <pgunary@gmail.com>
2025-04-01 09:16:15 +0100

    Implemented the sort order, added sorting to the plates.

--- src/BuildingBlocks/EventBus/IntegrationEvents/GetPlatesEvent.cs
+++ src/BuildingBlocks/EventBus/IntegrationEvents/GetPlatesEvent.cs
@@ -1,9 +1,15 @@
 using RTCodingExercise.Microservices.BuildingBlocks.EventBus.IntegrationEvents.Models;
+using EventBus.Enums;
 
 namespace RTCodingExercise.Microservices.BuildingBlocks.EventBus.IntegrationEvents
 {
     public class GetPlatesEvent : IntegrationEvent
     {
-        public GetPlatesEvent() : base() { }
+        public SortField SortField { get; set; } = SortField.None;
+        public SortDirection SortDirection { get; set; } = SortDirection.Ascending;
+        public GetPlatesEvent() : base()
+        {
+            CorrelationId = Guid.NewGuid();
+        }
     }
 }
